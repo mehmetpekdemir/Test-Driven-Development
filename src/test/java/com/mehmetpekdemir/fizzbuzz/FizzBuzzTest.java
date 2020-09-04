cba22b9e36cfebd493219577dcc42ef6a0d0ec99
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-09-04 22:45:06 +0300

    Refactored FizzBuzz.

---
+++ src/test/java/com/mehmetpekdemir/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,55 @@
+package com.mehmetpekdemir.fizzbuzz;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.mehmetpekdemir.fizzbuzz.FizzBuzz;
+
+/**
+ * 
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+public class FizzBuzzTest {
+
+	private FizzBuzz fizzBuzz;
+
+	@BeforeEach
+	void setUp() {
+		fizzBuzz = new FizzBuzz();
+	}
+
+	@Test
+	void returnFizzWhenTheNumberIsDivededByThree() {
+		assertEquals("Fizz", fizzBuzz.calculate(3));
+	}
+
+	@Test
+	void returnBuzzWhenTheNumberIsDivededByFive() {
+		assertEquals("Buzz", fizzBuzz.calculate(5));
+	}
+
+	@Test
+	void returnFizzBuzzWhenTheNumberIsDividedByBothOfThreeAndFive() {
+		assertEquals("FizzBuzz", fizzBuzz.calculate(15));
+	}
+
+	@Test
+	void returnTheNumberItselfWhenTheNumberIsNotDivededAnyOfThreeAndFive() {
+		assertEquals("7", fizzBuzz.calculate(7));
+	}
+
+	@Test
+	void throwsIllegalArgumentExceptionWhenTheNumberIsLessThanOne() {
+		assertThrows(IllegalArgumentException.class, () -> fizzBuzz.calculate(-1));
+	}
+
+	@Test
+	void throwsIllegalArgumentExceptionWhenTheNumberIsGreaterThanHundred() {
+		assertThrows(IllegalArgumentException.class, () -> fizzBuzz.calculate(101));
+	}
+
+}
